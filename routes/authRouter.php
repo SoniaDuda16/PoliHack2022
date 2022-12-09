dc4a010cf4ff0a9e8c0bba55dd0d1b129e358a23
SoniaDuda16 <duda_sonia_maria@yahoo.com>
2022-12-10 00:00:07 +0200

    dsa

--- routes/authRouter.php
+++ routes/authRouter.php
@@ -1,14 +1,17 @@
 <?php
     include $_SERVER["DOCUMENT_ROOT"]."/views//auth/partial/header.php";
     if(isset($_GET['page'])){
+        if($_GET['page']=="login"){
+            include $_SERVER["DOCUMENT_ROOT"]."/views/auth/login.php";
+        }
         if($_GET['page']=="register"){
             include $_SERVER["DOCUMENT_ROOT"]."/views/auth/register.php";
         }
         else{
-            include $_SERVER["DOCUMENT_ROOT"]."/views/auth/login.php";
+            include $_SERVER["DOCUMENT_ROOT"]."/views/auth/home.php";
         }
     } else {
-        include $_SERVER["DOCUMENT_ROOT"]."/views/auth/login.php";
+        include $_SERVER["DOCUMENT_ROOT"]."/views/auth/home.php";
     }
     include $_SERVER["DOCUMENT_ROOT"]."/views/auth/partial/footer.php";
 ?>
