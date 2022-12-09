23b08c181c7194ebf4ad6525233111472afab578
kerika29 <melidaerika29@gmail.com>
2022-12-10 01:07:26 +0200

    fh

---
+++ views/auth/desprenoi.php
@@ -0,0 +1,113 @@
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
+        <a href="index.html" class="navbar-brand p-0">
+            <h1 class="m-0 text-primary"><i class="fa fa-tooth me-2"></i>DentCare</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="index.html" class="nav-item nav-link">Acasă</a>
+                <a href="about.html" class="nav-item nav-link active">Despre Noi</a>
+                <a href="service.html" class="nav-item nav-link">Servicii</a>
+    
+            <a href="appointment.html" class="btn btn-primary py-2 px-4 ms-3">Programează-te</a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Full Screen Search Start -->
+    <div class="modal fade" id="searchModal" tabindex="-1">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content" style="background: rgba(9, 30, 62, .7);">
+                <div class="modal-header border-0">
+                    <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex align-items-center justify-content-center">
+                    <div class="input-group" style="max-width: 600px;">
+                        <input type="text" class="form-control bg-transparent border-primary p-3" placeholder="Type search keyword">
+                        <button class="btn btn-primary px-4"><i class="bi bi-search"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Full Screen Search End -->
+
+
+    <!-- Hero Start -->
+    <div class="container-fluid bg-primary py-5 hero-header mb-5">
+        <div class="row py-3">
+            <div class="col-12 text-center">
+                <h1 class="display-3 text-white animated zoomIn">Despre Noi</h1>
+            </div>
+        </div>
+    </div>
+    <!-- Hero End -->
+
+
+    <!-- About Start AICI SCHIMBAM TEXTUL-->
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-7">
+                    <div class="section-title mb-4">
+                        <h5 class="position-relative d-inline-block text-primary text-uppercase">Despre Noi</h5>
+                        <h1 class="display-5 mb-0">Rețeaua ta de cabinete medicale disponibilă acum la un click distanță</h1>
+                    </div>
+                    <h4 class="text-body fst-italic mb-4">Înregistrează-te acum și: </h4>
+                    <p class="mb-3">Vezi cabinetele disponibile pentru serviciile de care ai nevoie</p>
+                    <p class="mb-3">Sortează programările în funcție de nevoile tale: alege-le pe cele mai rapide sau pe cele mai ieftine</p>
+                    <p class="mb-3">În caz de urgență, ai acces rapid la numerele de telefon destinate acestor situații</p>
+                    <p class="mb-3">Primește alerte în caz de epidemii, ca să te protejezi pe tine și pe cei dragi</p>
+                    <h4 class="text-body fst-italic mb-4">Înregistrează o clinică: </h4>
+                    <p class="mb-3">Adaugă datele disponibile pentru programări</p>
+                    <p class="mb-3">Ai acces mai ușor la pacienți și la fișele medicale</p>
+                    <p class="mb-3">Contribuie la sănătatea publică și trimite alerte când se înregistrează mai multe cazuri de boli transmisibile decât în mod obișnuit</p>
+                    <!--<div class="row g-3">
+                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.3s">
+                            <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Award Winning</h5>
+                            <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Professional Staff</h5>
+                        </div>
+                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.6s">
+                            <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>24/7 Opened</h5>
+                            <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Fair Prices</h5>
+                        </div>
+                    </div> -->
+                    <a href="index.php?page=register" class="btn btn-primary py-3 px-5 mt-4 wow zoomIn" data-wow-delay="0.6s">Înregistrează-te</a>
+                </div> 
+                <div class="col-lg-5" style="min-height: 500px;">
+                    <div class="position-relative h-100">
+                        <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="img/about-1.jpg" style="object-fit: cover;">
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+    
+
+    <!-- Footer Start -->
+    
+    <div class="container-fluid text-light py-4" style="background: #051225;">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-md-6 text-center text-md-start">
+                    <p class="mb-md-0">&copy; <a class="text-white border-bottom" href="#">Your Site Name</a>. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <p class="mb-0">Designed by <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a><br>
+                        Distributed by <a class="text-white border-bottom" href="https://themewagon.com">ThemeWagon</a>          
+                    
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
