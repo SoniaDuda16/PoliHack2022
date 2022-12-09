b2b87845f80167d0141324596b4e87da6f55cdc3
laulupp <lupp.laurentiu@gmail.com>
2022-12-09 19:26:23 +0200

    update auth.php

--- functions/auth.php
+++ functions/auth.php
@@ -1,6 +1,13 @@
 <?php
 function isLoggedIn(){
+    if(isset($_SESSION['user']) && $_SESSION['user'] != null){
+        return true;
+    }
     return false;
+}
+function logOut(){
+    $_SESSION['user'] = null;
+    
 }
 
 ?>
