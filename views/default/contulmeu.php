87cef207eacf23329a757de0a674b1359114ff00
Mikaelis15 <mihaela.macarie01@icloud.com>
2022-12-10 22:33:26 +0200

    wed

---
+++ views/default/contulmeu.php
@@ -0,0 +1,23 @@
+<link href="public/css/account.css" rel="stylesheet">
+<div class="all">
+    <div class="buletin">
+        <table>
+            <tr>
+                <th> 
+                    <div class="poza">
+                        <img src="public/img/icon.png" width="150" height="150">
+                    </div>
+                </th>
+                <th>
+                    <div class="info">
+                        <h2> User </h2>
+                        <h6> Name:</h6>
+                        <h6> Email:</h6>
+                        <h6> Telefon:</h6>
+                        <h6> Adresa:</h6>
+                    </div>
+                </th>
+            </tr>
+        </table>
+    </div>
+</div>
--- views/default/partial/header.php
+++ views/default/partial/header.php
@@ -68,7 +68,6 @@
                 <button class="btn btn-primary py-2 ms-3"><?php echo $_SESSION['fullname'];?></button>
                 <div class="dropdown-content">
                     <a href="index.php?page=contulmeu">Contul meu</a> 
-                    <a href="#">Link 2</a>
                     <a href="index.php?page=logout">Logout</a>
                 </div>
             </div>
