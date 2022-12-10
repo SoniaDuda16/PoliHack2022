561b40434c31ffe4b4641b5ba6a988a39a4fffa8
SoniaDuda16 <duda_sonia_maria@yahoo.com>
2022-12-10 12:12:31 +0200

    update

--- views/auth/servicii.php
+++ views/auth/servicii.php
@@ -120,19 +120,17 @@
     </div>
     <!-- Service End -->
     
-
+<!-- Footer Start -->
     
-
-    <!-- Footer Start -->
-    <div class="container-fluid text-light py-2" style="background: #051225;">
+<div class="container-fluid text-light py-2" style="background: #051225;">
         <div class="container">
             <div class="row g-0">
                 <div class="col-md-6 text-center text-md-start">
                     <p class="mb-md-0">&copy; <a class="text-white border-bottom" href="#">Your Site Name</a>. All Rights Reserved.</p>
                 </div>
                 <div class="col-md-6 text-center text-md-end">
-                    <p class="mb-0"><a class="text-white border-bottom" href="https://htmlcodex.com"></a><br>
-                        <a class="text-white border-bottom" href="https://themewagon.com"></a>          
+                    <p class="mb-0">Designed by <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a><br>
+                        Distributed by <a class="text-white border-bottom" href="https://themewagon.com">ThemeWagon</a>          
                     
                     </p>
                 </div>
