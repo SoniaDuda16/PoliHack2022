bf8d12aafccfac065ca8f17235665f55473a4721
kerika29 <melidaerika29@gmail.com>
2022-12-10 10:54:03 +0200

    blabla

--- views/auth/home.php
+++ views/auth/home.php
@@ -42,17 +42,6 @@
         <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="public/img/carousel-2.jpg" alt="Image">
-                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <div class="p-3" style="max-width: 900px;">
-                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Ai grijă de tine!</h5>
-                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">Găsește rapid serviciile medicale de care ai nevoie</h1>
-                            <a href="/index.php?page=login" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Conectează-te</a>
-                            <a href="/index.php?page=register" class="btn btn-secondary py-md-3 px-md-5 animated slideInRight">Inregistrează-te</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
                     <img class="w-100" src="public/img/carousel-3.jpg" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
@@ -64,22 +53,28 @@
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#header-carousel"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
     </div>
     <!-- Carousel End -->
 
-
+<!-- Footer Start -->
     
+<div class="container-fluid text-light py-4" style="background: #051225;">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-md-6 text-center text-md-start">
+                    <p class="mb-md-0">&copy; <a class="text-white border-bottom" href="#">Your Site Name</a>. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <p class="mb-0">Designed by <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a><br>
+                        Distributed by <a class="text-white border-bottom" href="https://themewagon.com">ThemeWagon</a>          
+                    
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
