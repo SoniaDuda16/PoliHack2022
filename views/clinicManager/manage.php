a14f4bfc2f461888878fd92eb39f4c24eb6ac83b
antoniopotra <antonio.potra@gmail.com>
2022-12-10 23:49:38 +0200

    pt lau

--- views/clinicManager/manage.php
+++ views/clinicManager/manage.php
@@ -1,21 +1,34 @@
 <link href="public/css/manage.css" rel="stylesheet">
 
-<h1>Adaugă o nouă dată disponibilă</h1>
-<div class="slider">
-  <div class="slider-item">aisbcu</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
+<div class="add-zone">
+  <h5>Adaugă o nouă dată disponibilă</h5>
+  <form>
+    <div class="mb-3">
+      <input type="date" class="form-control" placeholder="Date">
+    </div>
+
+    <div class="mb-3">
+      <input type="time" class="form-control" placeholder="Time">
+    </div>
+
+    <div class="mb-3">
+      <input type="number" class="form-control" placeholder="Price">
+    </div>
+
+    <div class="mb-3">
+      <input type="text" class="form-control" placeholder="Specializare">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 </div>
 
-<h1>Programări viitoare</h1>
-<div class="slider">
-  <div class="slider-item">aisbcu</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
-  <div class="slider-item">iuasnb</div>
+<div class="see-zone">
+  <h1>Programări viitoare</h1>
+  <div class="see-appo">iuasnb</div>
+  <div class="see-appo">iuasnb</div>
+  <div class="see-appo">iuasnb</div>
+  <div class="see-appo">iuasnb</div>
+  <div class="see-appo">iuasnb</div>
+  <div class="see-appo">iuasnb</div>
 </div>
