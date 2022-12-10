a9aa1246626bd2095a03d437ffc0f292d1e02ae6
laulupp <lupp.laurentiu@gmail.com>
2022-12-10 15:51:34 +0200

    update clinici.php and urgente.php

---
+++ views/default/clinici.php
@@ -0,0 +1,51 @@
+<h4 class="mb-4"></h4>
+<div class="card shadow mb-4">
+    <div class="card-body p-5">
+        <!-- List with icons -->
+        <h4 class="mb-4">Clinica 1</h4>
+        <ul class="list-unstyled list pl-5">
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-map-marker mr-3 text-primary"></i> &nbsp; Adresa</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-search mr-3 text-primary"></i> &nbsp; Categorie</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-phone-square mr-3 text-primary"></i> &nbsp; Telefon</li>
+        </ul>
+    </div>
+</div>
+
+<div class="card shadow mb-4">
+    <div class="card-body p-5">
+        <!-- List with icons -->
+        <h4 class="mb-4">Cabinet 1</h4>
+        <ul class="list-unstyled list pl-5">
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-map-marker mr-3 text-primary"></i> &nbsp; Adresa</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-search mr-3 text-primary"></i> &nbsp; Categorie</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-phone-square mr-3 text-primary"></i> &nbsp; Telefon</li>
+        </ul>
+    </div>
+</div>
+
+<div class="card shadow mb-4">
+    <div class="card-body p-5">
+        <!-- List with icons -->
+        <h4 class="mb-4">Clinica 2</h4>
+        <ul class="list-unstyled list pl-5">
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-map-marker mr-3 text-primary"></i> &nbsp; Adresa</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-search mr-3 text-primary"></i> &nbsp; Categorie</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-phone-square mr-3 text-primary"></i> &nbsp; Telefon</li>
+        </ul>
+    </div>
+</div>
+
+<div class="card shadow mb-4">
+    <div class="card-body p-5">
+        <!-- List with icons -->
+        <h4 class="mb-4">Cabinet 2</h4>
+        <ul class="list-unstyled list pl-5">
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-map-marker mr-3 text-primary"></i> &nbsp; Adresa</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-search mr-3 text-primary"></i> &nbsp; Categorie</li>
+            <li class="mb-3 d-flex align-items-center"><i class="fa fa-phone-square mr-3 text-primary"></i> &nbsp; Telefon</li>
+        </ul>
+    </div>
+</div>
+
+<!-- List end -->
+<!-- Footer Start -->
