9d8b2d2289b9d4023979c3dc180c1d92348a3be8
antoniopotra <antonio.potra@gmail.com>
2022-12-10 22:55:17 +0200

    some manager stuff

---
+++ views/clinicManager/manage.php
@@ -0,0 +1,21 @@
+<link href="public/css/manage.css" rel="stylesheet">
+
+<h1>Adaugă o nouă dată disponibilă</h1>
+<div class="slider">
+  <div class="slider-item">aisbcu</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+</div>
+
+<h1>Programări viitoare</h1>
+<div class="slider">
+  <div class="slider-item">aisbcu</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+  <div class="slider-item">iuasnb</div>
+</div>
