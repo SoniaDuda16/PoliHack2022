06fc3572b14790f1901951a97b25da6d5b247f96
laulupp <lupp.laurentiu@gmail.com>
2022-12-10 12:28:05 +0200

    update auth.php, home.php, footer.php and header.php

---
+++ views/default/partial/footer.php
@@ -0,0 +1,41 @@
+<!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
+    
+    <!-- Footer Start -->
+    <div class="container-fluid text-light py-4" style="background: #051225;">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-md-6 text-center text-md-start">
+                    <p class="mb-md-0">&copy; <a class="text-white border-bottom" href="#">Your Site Name</a>. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <p class="mb-0"><a class="text-white border-bottom" href="https://htmlcodex.com"></a><br>
+                        <a class="text-white border-bottom" href="https://themewagon.com"></a>          
+                    
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="public/lib/wow/wow.min.js"></script>
+    <script src="public/lib/easing/easing.min.js"></script>
+    <script src="public/lib/waypoints/waypoints.min.js"></script>
+    <script src="public/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="public/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="public/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="public/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="public/lib/twentytwenty/jquery.event.move.js"></script>
+    <script src="public/lib/twentytwenty/jquery.twentytwenty.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="public/js/main.js"></script>
+</body>
+
+</html>
