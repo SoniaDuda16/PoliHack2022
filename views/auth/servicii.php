8fd2c591a525434e7ffb21b0bb2b4b972cc78c1e
SoniaDuda16 <duda_sonia_maria@yahoo.com>
2022-12-10 12:20:44 +0200

    rgr

--- views/auth/servicii.php
+++ views/auth/servicii.php
@@ -50,13 +50,13 @@
     <!-- Hero End -->
 
 
-    <!-- Service Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+   <!-- Service Start -->
+   <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container">
             <div class="row g-5 mb-5">
                 <div class="col-lg-5 wow zoomIn" data-wow-delay="0.3s" style="min-height: 400px;">
                     <div class="container position-relative h-100 rounded overflow-hidden">
-                        <img class="position-absolute w-100 h-100" src="public/img/doc.jpg" style="object-fit: cover;">
+                        <img class="position-absolute w-100 h-100" src="img/doc.jpg" style="object-fit: cover;">
                         
                     </div>
                 </div>
@@ -68,7 +68,7 @@
                     <div class="row g-5">
                         <div class="col-md-6 service-item wow zoomIn" data-wow-delay="0.6s">
                             <div class="rounded-top overflow-hidden">
-                                <img class="img-fluid" src="public/img/ofta.jpg" alt="">
+                                <img class="img-fluid" src="img/ofta.jpg" alt="">
                             </div>
                             <div class="position-relative bg-light rounded-bottom text-center p-4">
                                 <h5 class="m-0">Oftalmologie</h5>
@@ -76,7 +76,7 @@
                         </div>
                         <div class="col-md-6 service-item wow zoomIn" data-wow-delay="0.9s">
                             <div class="rounded-top overflow-hidden">
-                                <img class="img-fluid" src="public/img/service-2.jpg" alt="">
+                                <img class="img-fluid" src="img/service-2.jpg" alt="">
                             </div>
                             <div class="position-relative bg-light rounded-bottom text-center p-4">
                                 <h5 class="m-0">Stomatologie</h5>
@@ -90,7 +90,7 @@
                     <div class="row g-5">
                         <div class="col-md-6 service-item wow zoomIn" data-wow-delay="0.3s">
                             <div class="rounded-top overflow-hidden">
-                                <img class="img-fluid" src="public/img/derma.jpg" alt="">
+                                <img class="img-fluid" src="img/derma.jpg" alt="">
                             </div>
                             <div class="position-relative bg-light rounded-bottom text-center p-4">
                                 <h5 class="m-0">Dermatologie</h5>
@@ -98,7 +98,7 @@
                         </div>
                         <div class="col-md-6 service-item wow zoomIn" data-wow-delay="0.6s">
                             <div class="rounded-top overflow-hidden">
-                                <img class="img-fluid" src="public/img/familie.jpg" alt="">
+                                <img class="img-fluid" src="img/familie.jpg" alt="">
                             </div>
                             <div class="position-relative bg-light rounded-bottom text-center p-4">
                                 <h5 class="m-0">Medicină de familie</h5>
@@ -110,7 +110,7 @@
                 <div class="col-lg-5 service-item wow zoomIn" data-wow-delay="0.9s">
                     <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-4">
                         <h3 class="text-white mb-3">Descoperă toate serviciile pe care ți le oferim</h3>
-                        <p class="text-white mb-3">Programează-te acum</p>
+                        <a href="/index.php?page=register" class="btn btn-border-white btn-primary py-2 px-4 ms-3">Programează-te</a>
                         <h2 class="text-white mb-0"></h2>
                     </div>
                 </div>
