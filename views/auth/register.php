d7a1277fb896b49b4ecf596e3adfc30fc1fdd7f8
laulupp <lupp.laurentiu@gmail.com>
2022-12-10 09:56:29 +0200

    update register.php

--- views/auth/register.php
+++ views/auth/register.php
@@ -10,7 +10,7 @@
 			</div>
       
 			<div class="card-body">
-				<form action = "/index.php?page=register?try=reg" method = "POST">
+				<form action = "/index.php?page=register&try=reg" method = "POST">
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
